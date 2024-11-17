d0c33b8323f7347ce8d6128144cd15b892f6cc73
Henry Arnold <devharnold@gmail.com>
2024-11-17 12:08:02 +0300

    Code: working on the tests

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,72 @@
+#!usr/bin/env python3
+# test for the user model
+import pytest
+import psycopg2
+import engine
+from engine.db_storage import get_db_connection
+import engine.db_storage
+from models.baseModel import BaseModel
+from models.user import User
+import pep8
+from datetime import datetime
+import inspect
+
+class TestUserDocs:
+    """Test to check the documentation style of the user class"""
+    @classmethod
+    def setUpClass(cls):
+        """Set up for the doc test class"""
+        cls.user_f = inspect.getmembers(User, inspect.isfunction)
+
+    def test_pep8_conformance_user(self):
+        """tests to check if the user class docs conforms to pep8"""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['models/user.py'])
+        assert (result.total_errors, 0, "Found code style errors (and warnings).")
+
+class TestUser:
+    """Tests for the user class"""
+    def test_is_subclass(self):
+        """tests that the user is a subclass of the basemodel"""
+        user = User()
+        assert isinstance(user, BaseModel)
+        assert True(hasattr(user, "id"))
+        assert True(hasattr(user, "created_at"))
+
+    def test_has_email_attr(self):
+        """tests that the user model has email attribute"""
+        user = User()
+        assert True(hasattr(user, "email"))
+        if engine.db_storage == 'db':
+            self.assertEqual(user.email, None)
+        else:
+            self.assertEqual(user.email, "")
+
+    def test_has_first_name_attr(self):
+        """tests that the user model has the first name attribute"""
+        user = User()
+        assert True(hasattr(user, "first_name"))
+        if engine.db_storage == 'db':
+            self.assertEqual(user.first_name, None)
+        else:
+            self.assertEqual(user.first_name, "")
+
+    def test_has_last_name_attr(self):
+        """tests that the user model has last name attribute"""
+        user = User()
+        assert True(hasattr(user, "last_name"))
+        if engine.db_storage == 'db':
+            self.assertEqual(user.last_name, None)
+        else:
+            self.assertEqual(user.last_name, "")
+
+    def test_has_password_attr(self):
+        """tests that the user model has password attribute"""
+        user = User()
+        assert True(hasattr(user, "password"))
+        if engine.storage == 'db':
+            self.assertEqual(user.password, None)
+        else:
+            self.assertEqual(user.password, "")
+
+    def test_has
