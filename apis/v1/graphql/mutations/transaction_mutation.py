552fe7b867eb588c525c1a6d7dc5cbff850759d0
Henry Arnold <devharnold@gmail.com>
2024-12-27 19:00:39 +0300

    Code: Changes on User, Tranasaction Mutations

--- apis/v1/graphql/mutations/transaction_mutation.py
+++ apis/v1/graphql/mutations/transaction_mutation.py
@@ -2,6 +2,7 @@ import graphene
 from apis.v1.graphql.types import TransactionType
 from graphene import Field
 from engine.db_storage import get_db_connection
+import random
 
 class CreateTransaction(graphene.Mutation):
     class Arguments:
@@ -17,11 +18,35 @@ class CreateTransaction(graphene.Mutation):
 
         transaction = Field(lambda: TransactionType)
 
-        def mutate_transaction(self, info, sender_user_email, receiver_user_email, sender_user_id, receiver_user_id, sender_cw_id, receiver_cw_id, sender_currency, receiver_currency, amount):
-            connection = get_db_connection()
-            cursor = connection.cursor()
+    def mutate_transaction(self, info, sender_user_email, receiver_user_email, sender_user_id, receiver_user_id, sender_cw_id, receiver_cw_id, sender_currency, receiver_currency, amount):
+        connection = get_db_connection()
+        cursor = connection.cursor()
 
-            transaction_id = self.generate_transaction_id(cursor)
+        transaction_id = self.generate_transaction_id(cursor)
 
-            insert_query = """
-                INSERT INTO transactions (sender_user_email, receiver_user_email, sender_user_id, receiver_user_id, sender_cw_id)"""
+        insert_query = """
+            INSERT INTO transactions (sender_user_email, receiver_user_email, sender_user_id, receiver_user_id, sender_cw_id)"""
+        cursor.execute(insert_query, (sender_user_email, receiver_user_email, sender_user_id, receiver_user_id, sender_cw_id))
+        new_transaction = cursor.fetchone()
+
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+        return CreateTransaction(transaction=new_transaction)
+        
+    @staticmethod
+    def generate_transaction_id(cursor):
+        """Generate a unique random 10-digit transaction id"""
+        while True:
+            transaction_id = ''.join(str(random.randint(0, 9))for _ in range(10))
+
+            cursor.execute("SELECT transaction_id FROM transactions WHERE transaction_id = %s", (transaction_id))
+            existing_id = cursor.fetchone()
+
+            if not existing_id:
+                return transaction_id
+                
+
+class Mutation(graphene.ObjectType):
+    create_transaction = CreateTransaction.Field()
--- apis/v1/graphql/mutations/user_mutation.py
+++ apis/v1/graphql/mutations/user_mutation.py
@@ -3,6 +3,8 @@
 import graphene
 from apis.v1.graphql.types import UserType
 from graphene import String, Int, Field
+from engine.db_storage import get_db_connection
+import random
 
 
 class CreateUser(graphene.ObjectType):
@@ -15,16 +17,39 @@ class CreateUser(graphene.ObjectType):
 
     user = graphene.Field(lambda: UserType)
 
-    def mutate(self, info, user_id, first_name, last_name, user_email, phone_number, password):
-        new_user = {
-            "user_id": user_id,
-            "first_name": first_name,
-            "last_name": last_name,
-            "user_email": user_email,
-            "phone_number": phone_number,
-            "password": password
-        }
-        return CreateUser(new_user)
+    def mutate_user(self, info, first_name, last_name, user_email, phone_number, password):
+        connection = get_db_connection()
+        cursor = connection.cursor()
+
+        user_id = self.generate_user_id(cursor)
+
+        insert_query = """
+            INSERT INTO users (user_id, first_name, last_name, user_email, phone_number, password)
+            VALUES (%s, %s, %s, %s, %s, %s, CURRENT_TIMESTAMP)
+            RETURNING user_id, first_name, last_name, user_email, phone_number, password;
+            """
+        cursor.execute(insert_query, (user_id, first_name, last_name, user_email, phone_number, password))
+        new_user = cursor.fetchone()
+
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+
+        return CreateUser(new_user=CreateUser)
     
+    @staticmethod
+    def generate_user_id(cursor):
+        """Generate a random unique 10-digit user_id"""
+        while True:
+            user_id = ''.join(str(random.randint(0, 9))for _ in range(10))
+
+            # check if the user_id already exists in the database
+            cursor.execute("SELECT user_id FROM users WHERE user_id = %s", (user_id))
+            existing_id = cursor.fetchone()
+
+            if not existing_id:
+                return user_id
+            
 class Mutation(graphene.ObjectType):
     create_user = CreateUser.Field()
