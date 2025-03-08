47ca090ed3176a9ef83d230a7072b17ddc25c183
Henry Arnold <devharnold@gmail.com>
2025-03-08 10:27:04 +0300

    Change default backend file

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 from flask import Flask, jsonify
 import os
 from engine.db_storage import (
@@ -6,56 +7,34 @@ from engine.db_storage import (
     create_accounts_table,
     create_cashwallets_table,
     create_transactions_table,
-    #create_users_table,
-    create_fraud_detection_table,
+    create_fraud_detection_table
 )
 
 app = Flask(__name__)
 
 @app.route("/")
-def startApp():
-    return (f"Hello World!")
-
-@app.route("/")
 def create_tables():
-    """ Function to create db tables if they don't exist """
+    # Function to create db tables if they don't exist
     connection = get_db_connection()
     cursor = connection.cursor()
 
     try:
-        cursor.execute(create_accounts_table())
-        cursor.execute(create_cashwallets_table())
-        cursor.execute(create_transactions_table())
-        #cursor.execute(create_users_table())
-        cursor.execute(create_fraud_detection_table())
+        cursor.execute(create_accounts_table)
+        cursor.execute(create_cashwallets_table)
+        cursor.execute(create_transactions_table)
+        cursor.execute(create_fraud_detection_table)
 
         connection.commit()
-        print("Tables created successfully!")
-
+        print("All tables created successfully")
     except Exception as e:
-        print(f"Error creating tables: {e}")
+        print(f"Error creating tables: Error!")
         connection.rollback()
-
-    finally:
-        cursor.close()
-        connection.close()
 
 @app.route('/')
 def home():
-    #connecting to our db
+    # connect to the database
     connection = get_db_connection()
-    cursor = connection.cursor()
-
-    #execute a raw sql query
-    cursor.execute('SELECT version();')
-
-    db_version = cursor.fetchone()
-
-    #close the connection
-    cursor.close()
-    connection.close()
-
-    return jsonify({'PostgreSQL version': db_version[0]})
+    cursor = connection.cursor
 
 if __name__ == "__main__":
     create_tables()
