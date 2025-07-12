1b336955e3003269a2cc5564f0e12d4102768cd8
Henry Arnold <devharnold@gmail.com>
2025-07-13 00:28:23 +0300

    Correct and Debug Imports. Still yet to complete on correcting them.

--- backend/transactionService/grpc_client/wallet_client.py
+++ backend/transactionService/grpc_client/wallet_client.py
@@ -1,5 +1,5 @@
 import grpc
-from walletService.grpc_protos import wallet_pb2, wallet_pb2_grpc
+from backend.walletService.grpc_protos import wallet_pb2, wallet_pb2_grpc
 
 def get_wallet(wallet_id):
     with grpc.insecure_channel('localhost:50052') as channel:
@@ -7,4 +7,17 @@ def get_wallet(wallet_id):
         request = wallet_pb2.WalletRequest(wallet_id=wallet_id)
         response = stub.GetWallet(request)
         return response.wallet_details
-    
+
+def debit_wallet(wallet_id):
+    with grpc.insecure_channel('localhost:50052') as channel:
+        stub = wallet_pb2_grpc.WalletServiceStub(channel)
+        request = wallet_pb2.WalletRequest(wallet_id=wallet_id)
+        response = stub.DebitWallet(request)
+        return response.balance
+
+def credit_wallet(wallet_id):
+    with grpc.insecure_channel('localhost:50052') as channel:
+        stub = wallet_pb2_grpc.WalletServiceStub(channel)
+        request = wallet_pb2.WalletRequest(wallet_id=wallet_id)
+        response = stub.CreditWallet(request)
+        return response.balance
--- backend/walletService/grpc_server/wallet_grpc_server.py
+++ backend/walletService/grpc_server/wallet_grpc_server.py
@@ -1,13 +1,13 @@
 import grpc
 from concurrent import futures
-from grpc_protos import wallet_pb2_grpc
-from grpc_server.walletServiceimpl import WalletService
+from backend.walletService.grpc_protos import wallet_pb2_grpc
+from backend.walletService.grpc_server.walletServiceimpl import WalletService
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     wallet_pb2_grpc.add_WalletServiceServicer_to_Server(WalletService(), server)
     server.add_insecure_port('[::]:50052')
-    print("Walet service running on port 50052")
+    print("Wallet service running on port 50052")
     server.start()
     server.wait_for_termination()
 
